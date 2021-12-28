7d05787131f9339f125bfb2f258909b7b76d3725
BlackWidmoYu <filipdominiak00@gmail.com>
2021-12-28 17:31:49 +0100

    first  project is done

--- my-static-page-app/src/App.js
+++ my-static-page-app/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import './App.css';
-import Footer from "./components/Footer"
+
 import Nav from "./components/Nav"
 import MainContent from "./components/MainContent"
 export default function App() {
   return(<>
   <Nav />
   <MainContent />
-  <Footer />
+  
   </>)
 }
